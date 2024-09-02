0645eca0b5d53f32ee978e530442beb9aab1c711
Manuela Araújo <manuelafbaraujo@gmail.com>
2024-09-01 21:49:05 -0300

    docstring editado

--- validaTelefone.py
+++ validaTelefone.py
@@ -6,17 +6,7 @@ def validarTelefone(telefone):
     
     1. (xx) 9xxxx-xxxx
     2. (xx) 9xxxxxxxx
-    3. xx 9xxxxxxxx
-
-    Exemplos de números aceitos:
-    - "(91) 99999-9999"
-    - "(91) 999999999"
-    - "91 999999999"
-    
-    Exemplos de números rejeitados:
-    - "(91) 59999-9999" 
-    - "99 99999-9999"   
-    - "(94) 95555-5555"  
+    3. xx 9xxxxxxxx 
 
     Args: 
         telefone (string): número de telefone que será validado
