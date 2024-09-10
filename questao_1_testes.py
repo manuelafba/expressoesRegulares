80b4ae9e505afc1f1933c9377f8500d2b5ce38bb
Carolina de Lima Alves <carol.lima.alves2005@gmail.com>
2024-09-10 11:01:07 -0300

    limpeza de arquivos desnecessários

--- questao_1_testes.py
+++ questao_1_testes.py
@@ -1,121 +1,57 @@
 from questao_1_funcoes import *
 
-# Mensagem de boas-vindas
-print("Bem-vindo ao programa de validação de cadastro!!!")
-print("Vamos iniciar o cadastro")
 
 # Validação do nome
-print("""
-Digite o nome. Ele deve possuir primeiro e último nome, nomes intermediários são opcionais.
-Cada nome deve começar com uma letra maiúscula e ser seguido por letras minúsculas.
-Não devem conter símbolos nem números.\n""")
+print("Validação do nome")
+nomes = ['Ada Lovelace', 'Alan Turing', 'Stephen Cole Kleene', '1Alan', 'Alan', 'A1an', 'Alan turing', 'A1an Turing']
+for nome in nomes:
+    print(f'Nome: {nome.ljust(20)} - Válido: {validar_nome(nome)}')
+print()
 
-while True:
-    tentativa_nome = input("Digite aqui o seu nome: ")
-    resultado = validar_nome(tentativa_nome)
-    if not resultado:
-        print("Nome inválido, tente novamente\n")
-    else:
-        print("Nome válido\n")
-        break
 
 # Validação do email
-print("""
-Digite seu email. Ele deve conter apenas um '@', uma ou mais letras minúsculas antes e depois do '@',
-possuir apenas letras minúsculas e terminar com '.com.br' ou '.br'.\n""")
+print("Validação do email")
+emails = ['a@a.br', 'divulga@ufpa.br', 'a@a.com.br', '@', 'a@.br', '@a.br', 'T@teste.br', 'a@A.com.br', 'teste.a@ufpa.com']
+for email in emails:
+    print(f'Email: {email.ljust(20)} - Válido: {validar_email(email)}')
+print()
 
-while True:
-    tentativa_email = input("Digite aqui o seu email: ")
-    resultado = validar_email(tentativa_email)
-    if not resultado:
-        print("Email inválido, tente novamente\n")
-    else:
-        print("Email válido\n")
-        break
 
 # Validação da senha
-print("""
-Digite sua senha. Ela pode conter apenas letras maiúsculas, minúsculas e números.
-Deve conter pelo menos uma letra maiúscula, uma letra minúscula e ter no mínimo 8 caracteres.\n""")
+print("Validação do senha")
+senhas = ['518R2r5e', 'F123456A', '1234567T', 'ropsSoq0', 'F1234567A', 'abcdefgH', '1234567HI', 'aG9089']
+for senha in senhas:
+    print(f'Senha: {senha.ljust(20)} - Válido: {validar_senha(senha)}')
+print()
 
-while True:
-    tentativa_senha = input("Digite aqui a sua senha: ")
-    resultado = validar_senha(tentativa_senha)
-    if not resultado:
-        print("Senha inválida, tente novamente\n")
-    else:
-        print("Senha válida\n")
-        break
 
-# Validação do CPF
-print("""
-Digite o seu CPF. Ele deve ter o formato xxx.xxx.xxx-xx, com 'x' sendo números.\n""")
+# Validação do cpf
+print("Validação do cpf")
+cpfs = ['123.456.789-09', '000.000.000-00', '123.456.789-0', '111.111.11-11', '11109078645', '345876129-00', '098.678.12390']
+for cpf in cpfs:
+    print(f'CPF: {cpf.ljust(20)} - Válido: {validar_cpf(cpf)}')
+print()
 
-while True:
-    tentativa_cpf = input("Digite aqui o seu CPF: ")
-    resultado = validar_cpf(tentativa_cpf)
-    if not resultado:
-        print("CPF inválido, tente novamente\n")
-    else:
-        print("CPF válido\n")
-        break
 
 # Validação do telefone
-print("""
-Digite o seu número de telefone. Ele deve ter um dos seguintes formatos:
-  (xx) 9xxxx-xxxx
-  (xx) 9xxxxxxxx
-  xx 9xxxxxxxx
-com 'x' sendo números.\n""")
+print("Validação do telefone")
+telefones = ['(91) 99999-9999', '(91) 999999999', '91 999999999', '(91) 59999-9999', '99 99999-9999', '(94)95555-5555']
+for telefone in telefones:
+    print(f'Telefone: {telefone.ljust(20)} - Válido: {validar_telefone(telefone)}')
+print()
 
-while True:
-    tentativa_telefone = input("Digite aqui o seu telefone: ")
-    resultado = validar_telefone(tentativa_telefone)
-    if not resultado:
-        print("Telefone inválido, tente novamente\n")
-    else:
-        print("Telefone válido\n")
-        break
 
-# Validação da data e horário
-print("""
-Digite a sua data e horário no formato dd/mm/aaaa hh:mm:ss, onde d, m, a, h, m, s são números.\n""")
+# Validação da data e da hora
+print("Validação do data e da hora")
+datas = ['31/08/2019 20:14:55', '99/99/9999 23:59:59', '99/99/9999 3:9:9', '9/9/99 99:99:99', '99/99/999903:09:09', '9/99/9999 3:09:09', '99/9/9999 03:9:09', '99/99/99 03:09:9', '99999999 03:09:09','99/99/9999 030909',]
+for data in datas:
+    print(f'Data e hora: {data.ljust(20)} - Válido: {validar_data_hora(data)}')
+print()
 
-while True:
-    tentativa_data = input("Digite aqui a sua data e horário: ")
-    resultado = validar_data(tentativa_data)
-    if not resultado:
-        print("Data inválida, tente novamente\n")
-    else:
-        print("Data válida\n")
-        break
 
-# Validação do número real
-print("""
-Por fim, digite o seu número real. Ele deve seguir as seguintes regras:
-  1. Começar com um dos símbolos {+, -, ε}.
-  2. Seguir com, pelo menos, um número.
-  3. Ter exatamente um símbolo separador “.”.
-  4. Finalizar com, pelo menos, um número.
-  5. Exceção: números sem a parte fracionária também são aceitos.\n""")
-
-while True:
-    tentativa_real = input("Digite aqui o seu número real: ")
-    resultado = validar_real(tentativa_real)
-    if not resultado:
-        print("Número real inválido, tente novamente\n")
-    else:
-        print("Número real válido\n")
-        break
-
-# Finalização do cadastro
-print(f"""
-Cadastro finalizado! Seus dados são:
-  Nome: {tentativa_nome}
-  Email: {tentativa_email}
-  Senha: {tentativa_senha}
-  CPF: {tentativa_cpf}
-  Telefone: {tentativa_telefone}
-  Data e hora: {tentativa_data}
-  Número Real: {tentativa_real}
-""")
+# Validação do numero real
+print("Validação do numero real")
+numeros = ['1', '-1', '+1', '1.1', '-25.90', '0.2', '64.2', '1.', '.2', '+64,2']
+for numero in numeros:
+    print(f'Numero real: {numero.ljust(20)} - Válido: {validar_real(numero)}')
+print()
